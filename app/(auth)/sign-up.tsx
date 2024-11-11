10b78eee54c08e28bcfd98c6936bd82f984810ba
hemalmakani <hemalm@vt.edu>
2024-11-10 19:22:06 -0500

    Fixed keyboard blocking password field

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,4 +1,12 @@
-import { View, ScrollView, Image, Text, Alert } from "react-native";
+import {
+  View,
+  ScrollView,
+  Image,
+  Text,
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import { images } from "@/constants";
 import { icons } from "@/constants";
 import InputField from "@/components/InputField";
@@ -87,111 +95,119 @@ const SignUp = () => {
     }
   };
   return (
-    <ScrollView className="flex-1 bg-white">
-      <View className="flex-1 bg-white"></View>
-      <View className="relative w-full h-[250px]">
-        <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
-        <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
-          Create Your Account
-        </Text>
-      </View>
-      <View className="p-5">
-        <InputField
-          label="Name"
-          placeholder="Enter your name"
-          icon={icons.person}
-          value={form.name}
-          onChangeText={(value) => setFrom({ ...form, name: value })}
-        />
-        <InputField
-          label="Email"
-          placeholder="Enter your email"
-          icon={icons.email}
-          value={form.email}
-          onChangeText={(value) => setFrom({ ...form, email: value })}
-        />
-        <InputField
-          label="Password"
-          placeholder="Enter your password"
-          icon={icons.lock}
-          secureTextEntry={true}
-          value={form.password}
-          onChangeText={(value) => setFrom({ ...form, password: value })}
-        />
-        <CustomButton
-          title="Sign Up"
-          onPress={onSignUpPress}
-          className="mt-6"
-        />
-        <OAuth />
-        <Link
-          href="/sign-in"
-          className="text-lg text-center text-general-200 mt-10"
-        >
-          <Text>Already have an account? </Text>
-          <Text className="text-primary-500">Log In </Text>
-        </Link>
-      </View>
-
-      <ReactNativeModal
-        isVisible={verification.state === "pending"}
-        onModalHide={() => {
-          if (verification.state === "success") setShowSuccessModal(true);
-        }}
-      >
-        <View className="bg-white px-7 py-9 rounded-2xl min-h-[300px]">
-          <Text className="text2xl font-JakartaExtraBold mb-2 ">
-            Verification
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 20}
+    >
+      <ScrollView className="flex-1 bg-white">
+        <View className="flex-1 bg-white"></View>
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Create Your Account
           </Text>
-          <Text className="font-Jakarta mb-5">
-            We've sent a verification code to {form.email}
-          </Text>
-
+        </View>
+        <View className="p-5">
           <InputField
-            label="Code"
+            label="Name"
+            placeholder="Enter your name"
+            icon={icons.person}
+            value={form.name}
+            onChangeText={(value) => setFrom({ ...form, name: value })}
+          />
+          <InputField
+            label="Email"
+            placeholder="Enter your email"
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(value) => setFrom({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter your password"
             icon={icons.lock}
-            placeholder="12345"
-            value={verification.code}
-            keyboardType="numeric"
-            onChangeText={(code) => setVerification({ ...verification, code })}
+            secureTextEntry={true}
+            value={form.password}
+            onChangeText={(value) => setFrom({ ...form, password: value })}
           />
-          {verification.error && (
-            <Text className="text-red-500 text-sm mt-1">
-              {verification.error}
+          <CustomButton
+            title="Sign Up"
+            onPress={onSignUpPress}
+            className="mt-6"
+          />
+          <OAuth />
+          <Link
+            href="/sign-in"
+            className="text-lg text-center text-general-200 mt-10"
+          >
+            <Text>Already have an account? </Text>
+            <Text className="text-primary-500">Log In </Text>
+          </Link>
+        </View>
+
+        <ReactNativeModal
+          isVisible={verification.state === "pending"}
+          onModalHide={() => {
+            if (verification.state === "success") setShowSuccessModal(true);
+          }}
+        >
+          <View className="bg-white px-7 py-9 rounded-2xl min-h-[300px]">
+            <Text className="text2xl font-JakartaExtraBold mb-2 ">
+              Verification
             </Text>
-          )}
-          <CustomButton
-            title="Verify Email"
-            onPress={onPressVerify}
-            className="mt-5 bg-success-500"
-          />
-        </View>
-      </ReactNativeModal>
+            <Text className="font-Jakarta mb-5">
+              We've sent a verification code to {form.email}
+            </Text>
 
-      <ReactNativeModal isVisible={showSuccessModal}>
-        <View className="bg-white px-7 py-9 rounded-2xl min-h-[300px]">
-          <Image
-            source={images.check}
-            className="w-[110px] h-[110px] mx-auto my-5"
-          />
+            <InputField
+              label="Code"
+              icon={icons.lock}
+              placeholder="12345"
+              value={verification.code}
+              keyboardType="numeric"
+              onChangeText={(code) =>
+                setVerification({ ...verification, code })
+              }
+            />
+            {verification.error && (
+              <Text className="text-red-500 text-sm mt-1">
+                {verification.error}
+              </Text>
+            )}
+            <CustomButton
+              title="Verify Email"
+              onPress={onPressVerify}
+              className="mt-5 bg-success-500"
+            />
+          </View>
+        </ReactNativeModal>
 
-          <Text className="text-3xl font-JakartaBold text-center">
-            Verified
-          </Text>
-          <Text className="text-base text-gray-400 font-Jakarta text-center mt-2">
-            You have successfully verified your account.
-          </Text>
-          <CustomButton
-            title="Browse Home"
-            onPress={() => {
-              setShowSuccessModal(false);
-              router.push("/(root)/(tabs)/home");
-            }}
-            className="mt-5"
-          />
-        </View>
-      </ReactNativeModal>
-    </ScrollView>
+        <ReactNativeModal isVisible={showSuccessModal}>
+          <View className="bg-white px-7 py-9 rounded-2xl min-h-[300px]">
+            <Image
+              source={images.check}
+              className="w-[110px] h-[110px] mx-auto my-5"
+            />
+
+            <Text className="text-3xl font-JakartaBold text-center">
+              Verified
+            </Text>
+            <Text className="text-base text-gray-400 font-Jakarta text-center mt-2">
+              You have successfully verified your account.
+            </Text>
+            <CustomButton
+              title="Browse Home"
+              onPress={() => {
+                setShowSuccessModal(false);
+                router.push("/(root)/(tabs)/home");
+              }}
+              className="mt-5"
+            />
+          </View>
+        </ReactNativeModal>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
